d1384bc0d1b0258dbb31d97cfc051a0b9480d3de
Viet Cuong Le <VietCuong.Le@helmut-fischer.com>
2023-02-28 13:44:53 +0100

    infracstructure

---
+++ Demo.NetStandard.Infracstructure.XmlService.Impl/XmlContext.cs
@@ -0,0 +1,27 @@
+﻿using Demo.NetStandard.Core.Interfaces;
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Demo.NetStandard.Infracstructure.XmlService.Impl
+{
+	public class XmlContext: IUnitOfWork
+	{
+		public Task<IEnumerable<T>> SetAsync<T>()
+		{
+			throw new NotImplementedException();
+		}
+
+		public int SaveChanges(bool acceptAllChangesOnSuccess)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
