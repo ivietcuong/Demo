80d339b3947565e5f750e5ac103cb5c00fbb646b
Le Viet Cuong <le.vietcuong@outlook.com>
2023-03-01 22:20:05 +0100

    add content

--- src/3_UI/Wpf/Demo.Net.WpfApp/ViewModels/ShellViewModel.cs
+++ src/3_UI/Wpf/Demo.Net.WpfApp/ViewModels/ShellViewModel.cs
@@ -1,24 +1,21 @@
-﻿using Demo.Net.Wpf.Core;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+using Demo.Net.Wpf.Core;
 
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Demo.Net.WpfApp.ViewModels
 {
-    public class ShellViewModel
+    public partial class ShellViewModel : ObservableObject
     {
 
-        private object _content = null!;
-
-        public object Content
-        {
-            get { return _content; }
-            set { _content = value; }
-        }
+        [ObservableProperty]
+        private object? _content;
 
         public ShellViewModel(IEnumerable<IWorkspace> views)
         {
-            var xmlview = views.First();
+            Content = views.ElementAt(1);
         }
     }
 }
