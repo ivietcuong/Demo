75a113c54855ae49f602492deb3df43f82600fc0
Le Viet Cuong <le.vietcuong@outlook.com>
2023-03-05 00:12:28 +0100

    control

--- src/3_UI/Wpf/Demo.Net.WpfApp/ViewModels/HomeViewModel.cs
+++ src/3_UI/Wpf/Demo.Net.WpfApp/ViewModels/HomeViewModel.cs
@@ -1,24 +1,32 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 
 using Demo.NetStandard.Core.Data;
+using Demo.NetStandard.Core.Entities;
 
 using OxyPlot;
 using OxyPlot.Series;
 
+using System.Collections.Generic;
+
 namespace Demo.Net.WpfApp.ViewModels
 {
-	public partial class HomeViewModel : ObservableObject
-	{
+    public partial class HomeViewModel : ObservableObject
+    {
         [ObservableProperty]
         private PlotModel? _model;
+
+        [ObservableProperty]
+        public List<Point> _points = null!;
 
         public HomeViewModel()
         {
             var tmp = new PlotModel { Title = "Simple example", Subtitle = "using OxyPlot" };
-            var series = new LineSeries { Title = "Series"};
+            var series = new LineSeries { Title = "Series" };
             Data.Points.ForEach(p => { series.Points.Add(new DataPoint(p.X, p.Y)); });
             tmp.Series.Add(series);
             Model = tmp;
+
+            Points = new List<Point>(Data.Points);
         }
     }
 }
