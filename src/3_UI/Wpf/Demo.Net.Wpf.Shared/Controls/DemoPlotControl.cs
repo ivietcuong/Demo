f8e507e8ad91c70a39778b8e40153b2870c0705e
Le Viet Cuong <le.vietcuong@outlook.com>
2023-03-05 01:49:31 +0100

    ...

--- src/3_UI/Wpf/Demo.Net.Wpf.Shared/Controls/DemoPlotControl.cs
+++ src/3_UI/Wpf/Demo.Net.Wpf.Shared/Controls/DemoPlotControl.cs
@@ -12,17 +12,18 @@ namespace Demo.Net.Wpf.Shared.Controls
     [TemplatePart(Name = "PART_PlotView", Type = typeof(PlotView))]
     public class DemoPlotControl : Control
     {
+        public static readonly DependencyProperty PointsProperty =
+            DependencyProperty.Register("Points", typeof(List<DemoCore.Point>), typeof(DemoPlotControl), new PropertyMetadata(null, new PropertyChangedCallback(OnPointsChanged)));
+
         public static readonly DependencyProperty TitleProperty =
             DependencyProperty.Register("Title", typeof(string), typeof(DemoPlotControl), new FrameworkPropertyMetadata(string.Empty, new PropertyChangedCallback(OnTitleChanged)));
 
         public static readonly DependencyProperty SubtitleProperty =
             DependencyProperty.Register("Subtitle", typeof(string), typeof(DemoPlotControl), new FrameworkPropertyMetadata(string.Empty, new PropertyChangedCallback(OnSubTitleChanged)));
 
-        public static readonly DependencyProperty PointsProperty =
-            DependencyProperty.Register("Points", typeof(List<DemoCore.Point>), typeof(DemoPlotControl), new PropertyMetadata(default, new PropertyChangedCallback(OnPointsChanged)));
 
-        private PlotView _plotView = null!;
-        private LineSeries _lineSeries = null!;
+        private PlotView? _plotView;
+        private LineSeries? _lineSeries;
 
         public string Title
         {
@@ -69,7 +70,7 @@ namespace Demo.Net.Wpf.Shared.Controls
 
         private static void OnPointsChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
         {
-            DemoPlotControl control = (DemoPlotControl)dependencyObject;
+            var control = (DemoPlotControl)dependencyObject;
 
             if (control == null || control._plotView == null || e.NewValue == null)
                 return;
@@ -77,7 +78,7 @@ namespace Demo.Net.Wpf.Shared.Controls
 
         private static void OnTitleChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
         {
-            DemoPlotControl control = (DemoPlotControl)dependencyObject;
+            var control = (DemoPlotControl)dependencyObject;
 
             if (control == null || control._plotView == null || e.NewValue == null)
                 return;
@@ -85,7 +86,7 @@ namespace Demo.Net.Wpf.Shared.Controls
 
         private static void OnSubTitleChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
         {
-            DemoPlotControl control = (DemoPlotControl)dependencyObject;
+            var control = (DemoPlotControl)dependencyObject;
 
             if (control == null || control._plotView == null || e.NewValue == null)
                 return;
@@ -93,7 +94,7 @@ namespace Demo.Net.Wpf.Shared.Controls
 
         private static void OnSeriesTitleChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
         {
-            DemoPlotControl control = (DemoPlotControl)dependencyObject;
+            var control = (DemoPlotControl)dependencyObject;
 
             if (control == null || control._plotView == null || control._lineSeries == null || e.NewValue == null)
                 return;
