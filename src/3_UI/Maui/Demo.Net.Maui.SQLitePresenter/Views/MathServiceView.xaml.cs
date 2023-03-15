2cb9e64bd5287d641bc05b18b01ad1a9776ae70c
Viet Cuong Le <VietCuong.Le@helmut-fischer.com>
2023-03-15 17:01:47 +0100

    change color

--- src/3_UI/Maui/Demo.Net.Maui.SQLitePresenter/Views/MathServiceView.xaml.cs
+++ src/3_UI/Maui/Demo.Net.Maui.SQLitePresenter/Views/MathServiceView.xaml.cs
@@ -5,17 +5,17 @@ namespace Demo.Net.Maui.SQLitePresenter.Views;
 
 public partial class MathServiceView : ContentView, IWorkspace
 {
-    public string? Icon { get; set; }
-    public string? Description { get; set; }
+	public string? Icon { get; set; }
+	public string? Description { get; set; }
 
-    public MathServiceView()
-    {
-        InitializeComponent();
-    }
+	public MathServiceView()
+	{
+		InitializeComponent();
+	}
 
-    public MathServiceView(MathServiceViewModel viewModel)
-        : this()
-    {
-        BindingContext = viewModel;
-    }
+	public MathServiceView(MathServiceViewModel viewModel)
+		: this()
+	{
+		BindingContext = viewModel;
+	}
 }
